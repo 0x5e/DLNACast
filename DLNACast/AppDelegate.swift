a0f41a221779797425406dc0dcb126ee0331dd93
gaosen <0x5e@sina.cn>
2017-07-07 02:18:22 +0800

    Initial Commit

---
+++ DLNACast/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DLNACast
+//
+//  Created by 高森 on 2017/7/7.
+//  Copyright © 2017年 0x5e. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DLNACast/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DLNACast
+//
+//  Created by 高森 on 2017/7/7.
+//  Copyright © 2017年 0x5e. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
